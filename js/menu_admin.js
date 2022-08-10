6387c2731d455374f312e0297896548bed7ccdd5
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-10 16:01:01 +0200

    affichage des animaux des users et mise a jour de create et de update user

--- js/menu_admin.js
+++ js/menu_admin.js
@@ -31,6 +31,8 @@ function editUser(idDB, usernameDB, activeDB, roleDB, animalsString) { //, anima
 
     let divElement = document.getElementById("bloc_user_edit");
     let animals;
+    let animalsStringInit = animalsString;
+    let htmlAnimals = "";
     //let htmlAnimals = "";
     if (!isEditUserVisible) {
         
@@ -39,12 +41,22 @@ function editUser(idDB, usernameDB, activeDB, roleDB, animalsString) { //, anima
             animals = JSON.parse(animalsString);
             console.log("animal 1 : " + animals[0].name)
             console.log("animals string : " + animalsString);
+
+            htmlAnimals += "<ul class='ul_animals_list_user'>";
+            for(let i=0; i<animals.length; i++) { // animalType
+                htmlAnimals += "<li class='li_animals_list_user'>" 
+                + "<span class='white_text'>" + animals[i].name + "</span> / " 
+                + "<span class='orange_2_text'>" + animals[i].animalType.label + "</span> / " 
+                + "<span class='white_text'>" + animals[i].genre.toLowerCase() + "</span> / " 
+                + "<span class='orange_2_text'>" + animals[i].comment + "</span></li>";
+            }
+            htmlAnimals += "</ul>";
         }
         else {
             animals = null;
         }
 
-        // TODO checker les bonnes checkboxes selon les valeurs initiales
+        // TODO ajouter affichage des animaux
         let html = "";
         html += "<form id='form_user_add'>"
         
@@ -63,10 +75,11 @@ function editUser(idDB, usernameDB, activeDB, roleDB, animalsString) { //, anima
         + '<div class="champ_form" id="champ_password_edit">'
         + '<label for="password">Password</label>'
         + '<input class="input" type="text" id="password_edit" name="password">'
-        + '</div>'
-        
+        + '</div>';
+        html += htmlAnimals;
 
-        + '<div class="champ_form" id="champ_active">'
+
+        html +='<div class="champ_form" id="champ_active">'
         + '<label for="active">Active</label>'
         + '<input type= "radio" class="radio_user_add" id="true_radio_edit" name="active_edit" value="true" checked><span class= "text_radio_user_add">Oui</span>'
         + '<input type= "radio" class="radio_user_add" id="false_radio_edit" name="active_edit" value="false"><span class= "text_radio_user_add">Non</span>'
@@ -78,7 +91,7 @@ function editUser(idDB, usernameDB, activeDB, roleDB, animalsString) { //, anima
         + '<input type="radio" class="radio_user_add" id="admin_radio_edit" name="role_edit" value="admin"><span class= "text_radio_user_add">Admin</span>'
         + '</div>'
         + '<div class="champ_form" id="bloc_button_user_update">'
-        + '<button class="button" type="button" onclick=\'updateUserRequest(' + idDB  + ', "' + usernameDB + '" )\'>Mettre à jour cet user</button>'
+        + '<button class="button" type="button" onclick=\'updateUserRequest(' + idDB  + ', "' + usernameDB + '", "' + animalsStringInit + '" )\'>Mettre à jour cet user</button>'
         + '<button class="button" type="reset">Effacer</button>'
         + '</div>'
 
@@ -125,9 +138,7 @@ function editUser(idDB, usernameDB, activeDB, roleDB, animalsString) { //, anima
     }
 }
 
-// TODO modifier create et update pour la liste animals
-
-function updateUserRequest(idDB, usernameDB) {
+function updateUserRequest(idDB, usernameDB, animalsString) {
     //idDb = 
     // a faire avant la requete
     //let usernameDB = document.getElementById("username_edit").value;
@@ -139,6 +150,8 @@ function updateUserRequest(idDB, usernameDB) {
     let roleFormatted = getFormattedRole(roleDB.toLowerCase());
     let idRole = getRoleId(roleDB.toLowerCase());
 
+    let animalsStringOk = animalsString.replaceAll('|', '"');
+
     console.log("function updateUserRequest :");
     console.log("idDB :", idDB);
     console.log("usernameDB :", usernameDB);
@@ -146,8 +159,12 @@ function updateUserRequest(idDB, usernameDB) {
     console.log("activeDB :", activeFormatted);
     console.log("roleDB :", roleFormatted);
     console.log("idRole :", idRole);
+    console.log("animalsString :", animalsStringOk);
 
     // faire requete PUT
+    const animals = JSON.parse(animalsStringOk);
+
+    //console.log("animals : " + animals)
 
     if(passwordDB != "") {
         const roleContent = {
@@ -160,11 +177,13 @@ function updateUserRequest(idDB, usernameDB) {
             userName: usernameDB,
             password: passwordDB,
             active: activeFormatted,
-            role: roleContent
+            role: roleContent,
+            animals: animals
         };
 
         console.log("password ok et objet contentHeader créé");
 
+        
         fetch("http://localhost:8090/api/admin/update/" + idDB, {
             method: "PUT",
             headers: {
@@ -174,16 +193,7 @@ function updateUserRequest(idDB, usernameDB) {
             body: JSON.stringify(contentHeader)
         })
         .then(res => {
-            
-            // fermer les div et mettre a jour les booleens
             closeDivListUSer();
-            /*
-            resultElement = document.getElementById("result");
-            resultElement.innerHTML = "";
-            textButtonElement = document.getElementById("p_button_1");
-            textButtonElement.innerText="Afficher la liste des Users";
-            isUserListVisible = false;
-            */
             isUserListLoaded = false;
             document.getElementById("bloc_user_edit").innerHTML = "";
             isEditUserVisible = false;
@@ -281,7 +291,8 @@ function createUserRequest() { // localhost:8090/api/admin/create
         userName: usernameValue,
         password: passwordValue,
         active: activeValue,
-        role: roleContent
+        role: roleContent,
+        animals: []
     };
 
     fetch("http://localhost:8090/api/admin/create", {
@@ -320,6 +331,8 @@ function createUserRequest() { // localhost:8090/api/admin/create
     });
 }
 
+
+// TODO afficher la liste des animaux
 function requestAndDisplayUserInfos() {
     if (!isUserInfosVisible) {
         if(!isUserInfosLoaded) {
@@ -355,6 +368,7 @@ function requestAndDisplayUserInfos() {
     }
 }
 
+// TODO afficher la liste des animaux
 function displayUserInfos(data) {
     let html = "";
     let idDB = data.id;
@@ -363,11 +377,25 @@ function displayUserInfos(data) {
     let role = getRoleValue(data.role.label);
     let classNameActive = getClassActive(active);
     let classNameRole = getClassRole(data.role.label);
+    let animals =data.animals;
+    
     html += "<p>Id : <span class='orange_text'>" + idDB + "</span></p>";
     html += "<p>Username : <span class='orange_text'>" + username + "</span></p>";
     html += "<p>Password : <span id='password_info'>" + password + "</span></p>";
     html += "<p>Active : <span class='" + classNameActive + "'>" + active + "</span></p>";
     html += "<p>Rôle : <span class='" + classNameRole + "'>" + role + "</span></p>";
+    if(animals.length > 0) {
+        //console.log("animals : " + animals[0].name);
+        html += "<ul class='ul_animals_list_user'>";
+            for(let i=0; i<animals.length; i++) { // animalType
+                html += "<li class='li_animals_list_user'>" 
+                + "<span class='white_text'>" + animals[i].name + "</span> / " 
+                + "<span class='orange_2_text'>" + animals[i].animalType.label + "</span> / " 
+                + "<span class='white_text'>" + animals[i].genre.toLowerCase() + "</span> / " 
+                + "<span class='orange_2_text'>" + animals[i].comment + "</span></li>"
+            }
+            html += "</ul>";
+    }
     resultElement = document.getElementById("bloc_user_info");
     resultElement.innerHTML = html;
 }
@@ -420,10 +448,6 @@ function displayUserList(data) {
     resultElement = document.getElementById("result");
     let string ="<ul>";
     for (let i=0; i<data.length; i++) {
-        
-        
-
-
         let usernameDB = data[i].userName;
 
         let animals= data[i].animals;
@@ -434,9 +458,13 @@ function displayUserList(data) {
 
         // TODO faire fonction
         if(animalsString != "[]") {
-            htmlAnimals += "<ul>";
-            for(let i=0; i<animals.length; i++) {
-                htmlAnimals += "<li>" + animals[i].name + " / " + animals[i].comment  + " / " + animals[i].genre + "</li>"
+            htmlAnimals += "<ul class='ul_animals_list_user'>";
+            for(let i=0; i<animals.length; i++) { // animalType
+                htmlAnimals += "<li class='li_animals_list_user'>" 
+                + "<span class='white_text'>" + animals[i].name + "</span> / " 
+                + "<span class='orange_2_text'>" + animals[i].animalType.label + "</span> / " 
+                + "<span class='white_text'>" + animals[i].genre.toLowerCase() + "</span> / " 
+                + "<span class='orange_2_text'>" + animals[i].comment + "</span></li>"
             }
             htmlAnimals += "</ul>";
         }
@@ -503,6 +531,8 @@ function deleteUser(id) { ///admin/delete/{id} DELETE mapping
                 textButtonElement.innerText="Afficher la liste des Users";
                 isUserListVisible = false;
                 */
+                document.getElementById("bloc_user_edit").innerHTML = "";
+                isEditUserVisible = false;
                 isUserListLoaded = false;
                 alert("Suppression user ok : " + res.ok);
             })
